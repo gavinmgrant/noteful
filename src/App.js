311ea1bfa6044c1c4cdb5f0fa44d33f5a9aefb66
Gavin Grant <gavingrant@gmail.com>
2020-07-14 18:03:14 -0700

    installed react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import '.App.css';
 
-function App() {
-  return (
-    <main className='App'>
-      {/* content goes here */}
-    </main>
-  );
+class App extends Component {
+  render() {
+    return (
+      <main className='App'>
+        <header>
+          <h1>Noteful</h1>
+        </header>
+        <section className='mainSection'>
+
+        </section>
+        <aside className='sideBar'>
+
+        </aside>
+      </main>
+    );
+  }    
 }
 
 export default App;
